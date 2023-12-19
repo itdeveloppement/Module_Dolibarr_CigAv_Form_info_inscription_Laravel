522f1c941921d5abdd810cd7f385e811d9bc75af
MC6900200010 <marti.castellano@gmail.com>
2023-12-19 22:06:48 +0100

    push branch local

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,9 +1,4 @@
 const app = {
-
-
-    
-
-
     baseEndpoint: {
         endpoint: 'http://localhost:8000/api/inscriptions/1',
         // endpoint2: 'http://localhost:8000/api/inscriptions',
@@ -12,13 +7,11 @@ const app = {
 // appel des fonction a l'ouverture du document
 init: function() {
 
-    const id = document.location.pathname.split('/').pop(); // Récupère le dernier segment de l'URL
-    console.log(id);
-     console.log("test entree");
-
-     
+    // const id = document.location.pathname.split('/').pop(); // Récupère le dernier segment de l'URL
+    const $id = "2";
+    console.log($id);   
     console.log("test");
-    showData.init();
+    showData.init($id);
  
 }
 
